bc58da580825645e5a1ee470b7249972a8aad7f1
Joel Ambu <ambujoel@gmail.com>
2019-09-15 19:53:39 +0100

    Artistre Screen Done.

--- App.js
+++ App.js
@@ -1,12 +1,17 @@
 import React from "react";
 import { Provider } from "react-redux";
+
+import ArtisteScreen from "./screens/ArtisteScreen"
+
 import Rooter from "./config/Router";
 import store from "./config/createStore";
 
 export default function App() {
   return (
-    <Provider store={store}>
-      <Rooter />
-    </Provider>
+    
+    <ArtisteScreen />
+    // <Provider store={store}>
+    //   <Rooter />
+    // </Provider>
   );
 }
