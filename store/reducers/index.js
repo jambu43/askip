99e4ca69a70aa89f19aefb596d58282c138f346b
Junior Ngangeli <juniorngangeli3@gmail.com>
2020-02-19 05:53:13 +0100

    Implemented store for user management and podcasts.

--- store/reducers/index.js
+++ store/reducers/index.js
@@ -1,11 +1,13 @@
-import { combineReducers } from 'redux';
-import { authReducers } from './authReducers';
-import { magazinesReducer } from './magazinesReducer';
-import { podcastReducers } from './podcastReducers';
-import { newspaperReducers } from './newpaperReducers';
+import { combineReducers } from "redux";
+import { authReducers } from "./authReducers";
+import { magazinesReducer } from "./magazinesReducer";
+import { podcastReducers } from "./podcastReducers";
+import { newspaperReducers } from "./newpaperReducers";
+import { userReducer } from "./userReducer";
 export default combineReducers({
-	auth: authReducers,
-	magazine: magazinesReducer,
-	podcast: podcastReducers,
-	article: newspaperReducers
+  auth: authReducers,
+  magazine: magazinesReducer,
+  podcast: podcastReducers,
+  article: newspaperReducers,
+  user: userReducer,
 });
