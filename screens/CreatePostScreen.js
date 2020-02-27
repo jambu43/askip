64a74f2f5efd828d03a1f1661e2945b566dc22d9
Junior Ngangeli <juniorngangeli3@gmail.com>
2020-02-27 08:24:20 +0100

    [WIP]: create post screen

--- screens/CreatePostScreen.js
+++ screens/CreatePostScreen.js
@@ -3,12 +3,17 @@ import styled from "styled-components";
 import { connect } from "react-redux";
 import { dark } from "../config/variables";
 import AppHeader from "../components/generic/AppHeader";
+import UserAvatar from "../components/generic/UserAvatar";
 
 export default class CreatePostScreen extends React.Component {
   render() {
     return (
       <Container>
         <AppHeader />
+        <PostInputWrapper>
+          <UserAvatar />
+          <PostInput autofocus={true} placeholder="Quoi de neuf ?" />
+        </PostInputWrapper>
       </Container>
     );
   }
@@ -17,4 +22,12 @@ export default class CreatePostScreen extends React.Component {
 const Container = styled.View`
   flex: 1;
   background-color: ${dark};
+`;
+
+const PostInputWrapper = styled.View`
+  flex-direction: row;
+  padding: 15px;
+`;
+const PostInput = styled.TextInput`
+  margin-left: 15px;
 `;
