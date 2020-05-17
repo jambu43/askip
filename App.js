6309bcf97c2976fbcc74ce763e102d8c6cc0d090
Junior Ngangeli <juniorngangeli3@gmail.com>
2020-05-17 17:11:30 +0100

    Integrated google analytics

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import * as Analytics from "expo-firebase-analytics";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import Rooter from "./config/Router";
@@ -6,11 +7,38 @@ import moment from "./config/moment";
 import { store, persistor } from "./store/createStore";
 console.disableYellowBox = true;
 
+// Gets the current screen from navigation state
+const getActiveRouteName = (navigationState) => {
+  if (!navigationState) {
+    return null;
+  }
+  const route = navigationState.routes[navigationState.index];
+  // dive into nested navigators
+  if (route.routes) {
+    return getActiveRouteName(route);
+  }
+  return route.routeName;
+};
+
 export default function App() {
   return (
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <Rooter />
+        <Rooter
+          onNavigationStateChange={(prevState, currentState, action) => {
+            const currentScreen = getActiveRouteName(currentState);
+            const prevScreen = getActiveRouteName(prevState);
+            if (prevScreen !== currentScreen) {
+              console.log(currentScreen);
+              Analytics.setCurrentScreen(currentScreen, currentScreen);
+              Analytics.logEvent("share", {
+                contentType: "text",
+                itemId: "Expo rocks!",
+                method: "facebook",
+              });
+            }
+          }}
+        />
       </PersistGate>
     </Provider>
   );
