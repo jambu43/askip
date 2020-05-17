7c21bb183826755abc6fbf9b83d9c4da9c7d1c64
Junior Ngangeli <juniorngangeli3@gmail.com>
2020-05-17 16:18:17 +0100

    Implemented offline for auth and navigation.

--- App.js
+++ App.js
@@ -1,14 +1,17 @@
 import React from "react";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 import Rooter from "./config/Router";
 import moment from "./config/moment";
-import store from "./store/createStore";
+import { store, persistor } from "./store/createStore";
 console.disableYellowBox = true;
 
 export default function App() {
   return (
     <Provider store={store}>
-      <Rooter />
+      <PersistGate loading={null} persistor={persistor}>
+        <Rooter />
+      </PersistGate>
     </Provider>
   );
 }
--- store/createStore.js
+++ store/createStore.js
@@ -1,7 +1,16 @@
+import { AsyncStorage } from "react-native";
+import { persistStore, persistReducer } from "redux-persist";
 import { createStore, applyMiddleware } from "redux";
 import reducers from "./reducers";
 import thunk from "redux-thunk";
 
-const store = createStore(reducers, applyMiddleware(thunk));
+const persistConfig = {
+  key: "root",
+  storage: AsyncStorage,
+  whitelist: ["navigation", "auth"],
+};
 
-export default store;
+const persistedReducer = persistReducer(persistConfig, reducers);
+
+export const store = createStore(persistedReducer, applyMiddleware(thunk));
+export let persistor = persistStore(store);
