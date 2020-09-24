51f77dd01509df2682dfb69311423e0851d6c060
Junior Ngangeli <juniorngangeli3@gmail.com>
2020-09-24 07:16:16 +0100

    updated variables to target prod api.

--- helpers/index.js
+++ helpers/index.js
@@ -7,17 +7,17 @@ export const cutText = (text, length, more = "...") => {
 };
 
 export const apiUrl = (pathname) => {
-  return `http://askip.ngangeli.com/api/v1/${pathname}`;
+  return `http://askip.buzz/api/v1/${pathname}`;
 };
 
 export const assetsUrl = (pathname) => {
   if (pathname) {
     if (pathname.match("^file://")) return pathname;
     if (pathname.match("^http://") || pathname.match("^https://")) return pathname;
-    return `http://askip.ngangeli.com/storage/${pathname}`;
+    return `http://askip.buzz/storage/${pathname}`;
   }
 
-  return `http://askip.ngangeli.com/storage/`;
+  return `http://askip.buzz/storage/`;
 };
 
 export const simplePlural = (count, plural, singular) => {
